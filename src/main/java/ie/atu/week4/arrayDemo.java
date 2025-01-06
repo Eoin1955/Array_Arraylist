ec6ba44af05a675f675c20d78252c4beb63affdd
eoin <g00431577@atu.ie>
2025-01-06 02:04:36 +0000

    array of index 0 to 9

--- src/main/java/ie/atu/week4/arrayDemo.java
+++ src/main/java/ie/atu/week4/arrayDemo.java
@@ -1,5 +1,15 @@
 package ie.atu.week4;
 
-public class arrayDemo {
+import java.util.Scanner;
 
+public class arrayDemo {
+    public static void main(String[] args) {
+        int[] Array;
+        Array = new int[10];
+for(int i = 0; i < Array.length; i++){
+    System.out.print("Element at index ");
+    System.out.print(Array[i] + ":");
+    System.out.println(Array[i] = (i+1)*100);
+}
+    }
 }
